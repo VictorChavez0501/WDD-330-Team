da9b2ff0dfbc3d638ce21043ac18f7c309e5ef44
VictorChavez0501 <andreus4720.6@gmail.com>
2025-11-10 14:09:40 -0600

    Update cart.js
    Update the counter

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,11 @@
 import { getLocalStorage } from "./utils.mjs";
+import { updateCartCount } from "./cartCount.js";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  updateCartCount();
 }
 
 function cartItemTemplate(item) {
